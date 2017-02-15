53c8126cc83737890d93ca22cad9f5868d73a555
Yucheng Lin <Pike96@users.noreply.github.com>
2017-02-14 23:23:38 -0800

    Realize all

--- Q2.m
+++ Q2.m
@@ -1,6 +1,6 @@
 clear;
 
-n = 10;     % number of samples
+n = input('Please enter the number of samples: ');     % number of samples
 k = 4;
 for i = 1:n
     X(i) = sum(randn(1,k).^2);
@@ -18,3 +18,16 @@ ylim([0 1.05]);
 title('Chi-squared distribution of k=4');
 xlabel('x');
 ylabel('F(x)');
+
+F10x = 1/n:1/n:1;
+Fx = chi2cdf(X(1:n),k);
+disp(['The lower bound is: ', ...
+    num2str(max(F10x - Fx))]);
+
+disp(['25th percentile: ' num2str(X(floor(0.25*n))), ...
+    '   Theretical: ', num2str(chi2inv(0.25,4))]);
+disp(['50th percentile: ' num2str(X(floor(0.50*n))), ...
+    '   Theretical: ', num2str(chi2inv(0.50,4))]);
+disp(['95th percentile: ' num2str(X(floor(0.95*n))), ...
+    '   Theretical: ', num2str(chi2inv(0.95,4))]);
+ 

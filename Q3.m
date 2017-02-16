ecbb022587967b48b61e5015561caf268b6c6d47
Yucheng Lin <Pike96@users.noreply.github.com>
2017-02-15 22:43:06 -0800

    Realize all

--- Q3.m
+++ Q3.m
@@ -10,3 +10,10 @@ x = waiting(1:n);
 SEM = std(x)/sqrt(n);
 ts = tinv([0.025  0.975],n-1);
 CI = mean(x) + ts*SEM;
+disp(['The statistical confidence interval is: [',...
+    num2str(CI(1)),',   ',num2str(CI(2)),']']);
+
+% Bootstrap confidence interval
+b = sort(bootstrp(n, @mean, x));
+disp(['The bootstrap confidence interval is: [',...
+    num2str(b(ceil(n*0.025))),',    ',num2str(b(floor(n*0.975))),']']);
